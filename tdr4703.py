9ac070f751d2723ef6d3e61a5be7457bcd4d59f1
lincolncyber <grantrose1@gmail.com>
2022-08-04 12:02:01 -0400

    first

---
+++ tdr4703.py
@@ -0,0 +1,31 @@
+# This python script will log in to Geovision GV-TDR-4703 IP cameras and pull up the settings page.
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+ip = input('Enter Camera IP:')
+u = input('Enter username:')
+p = input('Enter Password:')
+http = 'http://'
+driver = webdriver.Chrome('chromedriver.exe')
+
+driver.get(http + ip)
+
+
+# driver.get("https://www.python.org")
+frame = driver.find_element_by_css_selector('iframe')
+driver.switch_to.frame(frame)
+
+user = driver.find_element_by_id('userName')
+password = driver.find_element_by_id('password')
+user.clear()
+user.send_keys(u)
+password.clear()
+password.send_keys(p)
+
+password.send_keys(Keys.RETURN)
+time.sleep(.5)
+
+
+driver.find_element_by_id('nav_config').click()
